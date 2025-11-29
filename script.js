9c4c1eb300c37383a58624c209fb558f4984008a
Teb-Houssem <tebourskihoussem@gmail.com>
2025-11-29 14:47:45 +0100

    Update script.js
    ajouter des effets sonnores et ajouter des categories pour tester les cps

--- script.js
+++ script.js
@@ -6,12 +6,30 @@ const timeDisplay = document.getElementById("time");
 const cpsDisplay = document.getElementById("cps");
 const bestDisplay = document.getElementById("best");
 const resetBtn = document.getElementById("buttonReset");
+const shootSound = document.getElementById("shootSound");
+const reload = document.getElementById("reload");
 let score = 0;
 let timeLeft = 0;
 let timer = null;
 let gameRunning = false;
 let bestScore = 0;
 
+
+function endTest(cps) {
+  let category = "";
+
+  if (cps < 3) {
+    category = "Slug 🐌";
+  } else if (cps < 6) {
+    category = "Casual 🙂";
+  } else if (cps < 8) {
+    category = "Average 😎";
+  } else {
+    category = "Flash ⚡";
+  }
+
+  alert(`Your CPS: ${cps}\nCategory: ${category}`);
+}
 startBtn.addEventListener("click", () => {
   const duration = Number(durationInput.value);
   if (!Number.isFinite(duration) || duration < 3 || duration > 120) {
@@ -43,8 +61,7 @@ startBtn.addEventListener("click", () => {
         bestDisplay.textContent = bestScore;
       }
 
-      alert("Temps écoulé ! Score final : " + score + 
-            " | CPS moyen : " + (score / duration).toFixed(2));
+      alert(endTest(cps));
     }
   }, 1000);
 });
@@ -52,6 +69,8 @@ startBtn.addEventListener("click", () => {
 punchBtn.addEventListener("click", () => {
   if (!gameRunning) return;
   score++;
+  shootSound.currentTime = 0; 
+  shootSound.play();
   scoreDisplay.textContent = score;
 });
 resetBtn.addEventListener("click", () => {
@@ -64,3 +83,4 @@ resetBtn.addEventListener("click", () => {
   cpsDisplay.textContent = "0.00";
   durationInput.value = "";
 });
+shootSound.volume = 0.6
